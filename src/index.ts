985c8ec4475dfa3a80474ec5ada35b8756c0925b
Shawn Meyer <sgmeyer@gmail.com>
2025-02-25 15:12:01 -0600

    Cleaning up code comments and dead code

--- src/index.ts
+++ src/index.ts
@@ -28,26 +28,6 @@ server.tool(
   }
 );
 
-// server.tool(
-//   "get-tenant-logs",
-//   "Fetches the Tenant Logs",
-//   { 
-//     page: z.number().optional().describe("Page index of the results to return. First page is 0."), 
-//     per_page: z.number().optional().describe("Number of results per page. Paging is disabled if parameter not sent. Default: <code>50</code>. Max value: <code>100</code>"),
-//     sort: z.string().optional().describe("Comma-separated list of fields to include or exclude (based on value provided for <code>include_fields</code>) in the result. Leave empty to retrieve all fields."),
-//     fields: z.string().optional().describe("Comma-separated list of fields to include or exclude (based on value provided for <code>include_fields</code>) in the result. Leave empty to retrieve all fields."),
-//     include_fields: z.boolean().optional().describe("Whether specified fields are to be included (<code>true</code>) or excluded (<code>false</code>)"),
-//     include_totals: z.boolean().optional().describe("Return results as an array when false (default). Return results inside an object that also contains a total result count when true."),
-//     from: z.string().optional().describe("This takes a Tenant Log Event Id from which to start selection from"),
-//     take: z.number().optional().describe("Number of entries to retrieve when using the <code>from</code> parameter. Default <code>50</code>, max <code>100</code>"),
-//     q: z.string().optional().describe("Query in <a target='_new' href ='http://www.lucenetutorial.com/lucene-query-syntax.html'>Lucene query string syntax</a>.")
-//   },
-//   async ({ page, per_page, sort, fields, include_fields, include_totals, from, take, q }) => {
-//     const logs = await auth0.logs.getAll({ page, per_page, sort, fields, include_fields, include_totals, from, take, q });
-//     return { content: [{ type: "text", text: JSON.stringify(logs) }] };
-//   }
-// );
-
 server.tool(
   "find-user-by-email",
   "Find a user for a given email",
